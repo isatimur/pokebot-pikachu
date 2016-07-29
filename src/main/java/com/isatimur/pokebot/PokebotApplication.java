5d9a5518f85200c7c439f70d840a2c97de5a56fe
Timur Isa <tisachenko@at-consulting.ru>
2016-07-29 22:56:01 +0300

    Initial commit

---
+++ src/main/java/com/isatimur/pokebot/PokebotApplication.java
@@ -0,0 +1,48 @@
+package com.isatimur.pokebot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+@SpringBootApplication
+public class PokebotApplication implements CommandLineRunner {
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(PokebotApplication.class.getSimpleName());
+
+    @Override
+    public void run(String... strings) throws Exception {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new TelegramLongPollingBot() {
+                @Override
+                public String getBotToken() {
+                    return "155693744:AAHm5hZ-yiCBZa6TWkBrrIiH4MQdg9g5kuA";
+                }
+
+                @Override
+                public void onUpdateReceived(Update update) {
+                    Message msg = update.getMessage();
+                    logger.info(msg.getText());
+                }
+
+                @Override
+                public String getBotUsername() {
+                    return "p_o_k_e_bot";
+                }
+            });
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PokebotApplication.class, args);
+
+    }
+}
